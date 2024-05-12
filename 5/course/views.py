16d189a4e3c56dcabf6c3cae35e528ab4e120de1
Sardor <165559483+Sardor013@users.noreply.github.com>
2024-05-12 12:36:02 +0500

    1

--- 5/course/views.py
+++ 5/course/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def about(request):
-    return HttpResponse("Biz hozirda python dasturlash kursida ta'lim olyapmiz")
+    return HttpResponse("Biz hozirda Python dasturlash kursida ta'lim olyapmiz")
 # Create your views here.
